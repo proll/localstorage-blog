25767e99302407be2ef95787802ebf53bb0e3d86
proll <gleb.proll@gmail.com>
2015-02-17 17:36:58 +0300

    heroku up

--- heroku_web.js
+++ heroku_web.js
@@ -1,72 +1,41 @@
-/*
- * grunt
- * http://gruntjs.com/
- *
- * Copyright (c) 2012 "Cowboy" Ben Alman
- * Licensed under the MIT license.
- * http://benalman.com/about/license/
- */
+// Nodejs libs.
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
 
-'use strict';
+// External libs.
+var connect = require('connect'),
+	httpProxy = require('http-proxy');
 
-module.exports = function(grunt) {
+// Get values from config, or use defaults.
+var port = process.env.PORT || 5000;
+var base = './dist';
+var proxy = new httpProxy.RoutingProxy({changeOrigin: true});
 
-	// Nodejs libs.
-	var path = require('path');
-	var fs = require('fs');
-	var url = require('url');
-
-	// External libs.
-	var connect = require('connect'),
-		httpProxy = require('http-proxy');
-
-	// ==========================================================================
-	// TASKS
-	// ==========================================================================
-
-	grunt.registerTask('server', 'Start a static web server.', function() {
-		// Get values from config, or use defaults.
-		var port = grunt.config('server.port') || 8000;
-		var base = path.resolve(grunt.config('server.base') || '.');
-
-		var proxy = new httpProxy.RoutingProxy({changeOrigin: true});
-
-		var middleware = [
-			// if request points to non existing file, route to index.html
-			function(req, res, next){
-				var requestedPath = url.parse(req.url).pathname;
-				fs.exists(base + requestedPath, function(exists){
-					if(exists){
-						return next();
+var middleware = [
+	// if request points to non existing file, route to index.html
+	function(req, res, next){
+		var requestedPath = url.parse(req.url).pathname;
+		fs.exists(base + requestedPath, function(exists){
+			if(exists){
+				return next();
+			}else{
+				fs.readFile(base + "/index.html", function(error, content){
+					if(error){
+						console.log("Cannot read file: " + base + "/index.html");
 					}else{
-						fs.readFile(base + "/index.html", function(error, content){
-							if(error){
-								console.log("Cannot read file: " + base + "/index.html");
-							}else{
-								res.writeHead(200, { 'Content-Type': 'text/html' });
-								res.end(content, "utf-8");
-							}
-						});
+						res.writeHead(200, { 'Content-Type': 'text/html' });
+						res.end(content, "utf-8");
 					}
 				});
-			},
-			
-			// Serve other files static
-			connect.static(base),
-			
-			// Make empty directories browsable. (overkill?)
-			connect.directory(base)
-		];
-
-		// If --debug was specified, enable logging.
-		if (grunt.option('debug')) {
-			connect.logger.format('grunt', ('[D] server :method :url :status ' +
-				':res[content-length] - :response-time ms').magenta);
-			middleware.unshift(connect.logger('grunt'));
-		}
-
-		// Start server.
-		grunt.log.writeln('Starting static web server on port ' + port + '.');
-		connect.apply(null, middleware).listen(port);
-	});
-};
+			}
+		});
+	},
+	
+	// Serve other files static
+	connect.static(base),
+	
+	// Make empty directories browsable. (overkill?)
+	connect.directory(base)
+];
+connect.apply(null, middleware).listen(port);
